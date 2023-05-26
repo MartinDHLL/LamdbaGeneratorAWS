5502f35b3f1b1dfff04574d013fc43984e359c4a
Martin Dhollande <mdhollande@fabriquenumerique.fr>
2023-05-26 09:13:35 +0200

    security improved

--- bin/cdk-demo.ts
+++ bin/cdk-demo.ts
@@ -2,6 +2,9 @@
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
 import { CdkLambdaStack } from "../lib/cdk-lambda-stack";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const app = new cdk.App();
 new CdkLambdaStack(app, "CdkLambdaStack", {
@@ -15,7 +18,7 @@ new CdkLambdaStack(app, "CdkLambdaStack", {
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  env: { account: "512433258031", region: "us-east-1" },
+  env: { account: process.env.ACCOUNT_NUMBER, region: process.env.REGION },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
